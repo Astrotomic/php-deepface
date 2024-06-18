fa84f122212a0fac43c6685f346656a949baad6c
Pablo Gabarro <pgabarro@parana.gob.ar>
2024-06-18 12:47:42 -0300

    Added anti_spoofing support

--- scripts/verify.py
+++ scripts/verify.py
@@ -1,15 +1,19 @@
 import json;
 from deepface import DeepFace;
 
-result = DeepFace.verify(
-    img1_path = "{{img1_path}}",
-    img2_path = "{{img2_path}}",
-    enforce_detection = {{enforce_detection}},
-    align = {{align}},
-    model_name = "{{model_name}}",
-    detector_backend = "{{detector_backend}}",
-    distance_metric = "{{distance_metric}}",
-    normalization = "{{normalization}}"
-);
+try:
+    result = DeepFace.verify(
+        img1_path = "{{img1_path}}",
+        img2_path = "{{img2_path}}",
+        enforce_detection = {{enforce_detection}},
+        anti_spoofing={{anti_spoofing}},
+        align = {{align}},
+        model_name = "{{model_name}}",
+        detector_backend = "{{detector_backend}}",
+        distance_metric = "{{distance_metric}}",
+        normalization = "{{normalization}}"
+    );
 
-print(json.dumps(result, default=str))
+    print(json.dumps(result, default=str))
+except ValueError as e:
+    print(json.dumps({"error": str(e)}), file=sys.stderr)
